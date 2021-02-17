9de568d2eeecb8bdfe2dd89c9a139ed55ce4f57f
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2021-02-17 15:47:44 +0100

    feat: add 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends("parts.main")
+
+<style>
+    h1 {
+        text-align: center;
+        margin-top: 100px;
+    }
+</style>
+
+@section('main')
+    <h1>Error 404, esta página no existe.</h1>
+@endsection
